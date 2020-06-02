52e46e979c531ae2cc18e2f654110e097cc688fd
Parkertinsley <parker.tinsley@gmail.com>
2020-06-02 08:09:51 -0600

    video 75

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-root 'pages#home'
-get 'about', to: 'pages#about' 
+  resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about' 
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
